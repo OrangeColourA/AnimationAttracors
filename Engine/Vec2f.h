d0e206a144f5b4a89086d4ba2ed8fa0b8509a763
Анатолий Козлов <Anatoliy.kozlov2001@gmail.com>
2024-11-15 10:21:16 +0300

    Added Vec2f

--- Engine/Vec2f.h
+++ Engine/Vec2f.h
@@ -10,10 +10,20 @@ public:
 	float x;
 	float y;
 
+public:
 	Vec2f() = default;
 	Vec2f(float x, float y);
 	Vec2f(const Vec2f& v);
-	
-
+	Vec2f& operator=(const Vec2f& r);
+	Vec2f  operator+(const Vec2f& r) const;
+	Vec2f& operator+=(const Vec2f& r);
+	Vec2f  operator-(const Vec2f& r) const;
+	Vec2f& operator-=(const Vec2f& r);
+	Vec2f  operator*(float num) const;
+	Vec2f& operator*=(float num);
+	float GetLengthSq() const;
+	float GetLength() const;
+	Vec2f GetNormalized() const;
+	Vec2f& Normalized();
 
 };
